f71475b44d7980676ca301afeab4a7ace545896f
coolnight664 <coolnight664@gmail.com>
2026-07-25 15:59:53 -0700

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import discordAuthPlugin from './discord-auth-plugin.js'
 
-export default defineConfig({
-  plugins: [react(), discordAuthPlugin()],
+export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/Nights.lol/' : '/',
+  plugins: [react(), ...(mode === 'development' ? [discordAuthPlugin()] : [])],
   server: {
     port: 3000,
     open: true
   }
-})
+}))
